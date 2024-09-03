83bfb963111888ec20aad3dbabb486d842fc7741
ParthaSarathiRautaray1 <145197547+ParthaSarathiRautaray1@users.noreply.github.com>
2024-09-03 20:53:50 +0530

    creating chat page with 3 fields and a image place and fname , lname should be same as where you move from one to another route

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -107,3 +107,42 @@ export const getUserInfo = async (request , response , next) =>{
         return response.status(500).send("Login Server Errorrr")
     }
 }
+
+
+
+export const updateProfile = async (request , response , next) =>{
+    try {
+
+        const { userId } = request
+        const { firstName , lastName , color } = request.body;
+        
+        if(!firstName || !lastName ){
+            return response
+            .status(400)
+            .send("First Name Last Name is Required...")
+        }
+
+        const userData = await User.findByIdAndUpdate(userId,{
+            firstName,lastName,color,profileSetup:true
+        },{new:true, runValidators:true}
+    )
+       
+        
+        return response.status(200).json({
+           
+                id: userData.id,
+                email: userData.email,
+                profileSetup: userData.profileSetup,
+                firstName : userData.firstName,
+                lastName: userData.lastName,
+                image: userData.image,
+                color:userData.color,
+
+         
+        });
+
+    }catch (error) {
+        console.log('Backend signup error:', error)
+        return response.status(500).send("Login Server Errorrr")
+    }
+}
